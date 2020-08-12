1165392a20db3d8bb1e7d755004e945a56fa25f4
sberoch <sberoch@gmail.com>
2020-08-12 19:44:40 -0300

    tests concurso

--- ewriters/src/test/groovy/ewriters/ConcursoSpec.groovy
+++ ewriters/src/test/groovy/ewriters/ConcursoSpec.groovy
@@ -6,60 +6,52 @@ import spock.lang.Specification
 class ConcursoSpec extends Specification implements DomainUnitTest<Concurso> {
 
 	def creadorDelConcurso
+    def usuario 
 
     def setup() {
     	creadorDelConcurso = new Usuario("Alice")
-    }
-
-    def cleanup() {
+        usuario = new Usuario("Bob")
     }
 
     void "test participacion exitosa en concurso"() {
         given: "un concurso"
-        	def concurso = new Concurso(creadorDelConcurso,
-        		"Concurso de Alice",
+        	def concurso = new Concurso("Concurso de Alice",
         		"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
         		200,
         		50,
         		Narracion.Genero.FANTASIA)
-        when: "se quiera participar con los requerimientos minimos"
-        	def participante = new Usuario("Bob")
-        	participante.reputacion = 100
-        	def narracion = new Narracion(participante,
-        		"Narracion de Bob",
+        when: "un usuario quiera participar con los requerimientos minimos"
+        	usuario.reputacion = 100
+        	def narracion = new Narracion("Narracion de Bob",
         		"Lorem ipsum dolor sit amet",
         		Narracion.Genero.FANTASIA)
-        	concurso.registrarParticipacion(narracion)
-        then: "se registrara exitosamente la narracion"
+        	concurso.agregarNarracion(narracion, usuario)
+        then: "se agregara exitosamente la narracion"
         	concurso.narraciones.contains(narracion)
     }
 
     void "test participacion fallida en concurso"() {
         given: "un concurso"
-        	def concurso = new Concurso(creadorDelConcurso,
+        	def concurso = new Concurso(
         		"Concurso de Alice",
         		"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
         		200,
         		50,
         		Narracion.Genero.FANTASIA)
         when: "se quiera participar sin los requerimientos minimos"
-        	def participante = new Usuario("Bob")
-        	participante.reputacion = 25
-        	def narracion = new Narracion(participante,
-        		"Narracion de Bob",
+        	usuario.reputacion = 25
+        	def narracion = new Narracion("Narracion de Bob",
         		"Lorem ipsum dolor sit amet",
         		Narracion.Genero.TERROR)
-        	concurso.registrarParticipacion(narracion)
+        	concurso.agregarNarracion(narracion, usuario)
         then: "no se podra participar con esa narracion"
-        	false == concurso.narraciones.contains(narracion)
         	def exception = thrown(IllegalStateException)
         	exception != null 
     }
 
     void "test el concurso dura una semana"() {
         given: "un concurso"
-            def concurso = new Concurso(creadorDelConcurso,
-                "Concurso de Alice",
+            def concurso = new Concurso("Concurso de Alice",
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
                 200,
                 50,
@@ -71,22 +63,22 @@ class ConcursoSpec extends Specification implements DomainUnitTest<Concurso> {
 
     void "test la narracion con mas me gusta gana el concurso"() {
         given: "un concurso y dos narraciones con diferente cantidad de me gusta"
-            def concurso = new Concurso(creadorDelConcurso,
-                "Concurso de Alice",
+            def otroUsuario = new Usuario("Charlie")
+            def concurso = new Concurso("Concurso de Alice",
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
                 200,
                 0,
                 Narracion.Genero.FANTASIA)
-            def narracion_1 = new Narracion(new Usuario("Bob"),
-                "Narracion de Bob",
+            def narracion_1 = new Narracion("Narracion de Bob",
                 "Lorem ipsum dolor sit amet",
                 Narracion.Genero.FANTASIA)
-            def narracion_2 = new Narracion(new Usuario("Charlie"),
-                "Narracion de Charlie",
+            def narracion_2 = new Narracion("Narracion de Charlie",
                 "Lorem ipsum dolor sit amet",
                 Narracion.Genero.FANTASIA)
-            concurso.registrarParticipacion(narracion_1)
-            concurso.registrarParticipacion(narracion_2)
+            narracion_1.escritor = usuario
+            narracion_2.escritor = otroUsuario
+            concurso.agregarNarracion(narracion_1, usuario)
+            concurso.agregarNarracion(narracion_2, otroUsuario)
             narracion_1.cantMeGusta = 3
             narracion_2.cantMeGusta = 5
         when: "se determine el ganador"
